33692f9199064dd59b0a3fe29e50e698bf964281
B45417 <162130704+B45417@users.noreply.github.com>
2025-03-26 23:44:14 +0100

    Update mrs

--- turnip-stable_builder.sh
+++ turnip-stable_builder.sh
@@ -15,14 +15,15 @@ mesaver="25.0.2"
 #array of string => commit/branch;patch args
 base_patches=(
         "bin_merging;merge_requests/33230;"
-	#"fix_gpu_faults;merge_requests/33274;"
         "images_gmem_fixes;merge_requests/33514;"
 	"bin_merging_fixes;merge_requests/33534;"
         "bypass_empty_fs;merge_requests/33735;"
+	"update_309;merge_requests/33930;"
 	"bc4_5_fast;merge_requests/33945;"
-        #"fragment_size_fix;merge_requests/33991;"
+        "fragment_size_fix;merge_requests/33991;"
 	"layer_count_fix;merge_requests/34080;"
 	"gmem_fix;merge_requests/34082;"
+        "update_311;merge_requests/34106;"
 )
 experimental_patches=(
         'disable_VK_KHR_workgroup_memory_explicit_layout;../../patches/disable_KHR_workgroup_memory_explicit_layout.patch;'
