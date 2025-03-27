f9b63e31bc750e7d45d694ec7fcb64d4dfbe49d3
B45417 <162130704+B45417@users.noreply.github.com>
2025-03-27 13:01:52 +0100

    Test build options

--- turnip-stable_builder.sh
+++ turnip-stable_builder.sh
@@ -185,6 +185,7 @@ c = ['ccache', '$ndk/aarch64-linux-android$sdkver-clang']
 cpp = ['ccache', '$ndk/aarch64-linux-android$sdkver-clang++', '-fno-exceptions', '-fno-unwind-tables', '-fno-asynchronous-unwind-tables', '--start-no-unused-arguments', '-static-libstdc++', '--end-no-unused-arguments']
 c_ld = '$ndk/ld.lld'
 cpp_ld = '$ndk/ld.lld'
+strip = '$ndk/llvm-strip'
 [host_machine]
 system = 'android'
 cpu_family = 'aarch64'
@@ -202,7 +203,9 @@ EOF
 		-Dvulkan-drivers=freedreno \
                 -Dvulkan-beta=true \
                 -Dgallium-drivers= \
-		-Dfreedreno-kmds=kgsl &> "$workdir"/meson_log
+		-Dfreedreno-kmds=kgsl \
+		-Db_lto=true \
+		-Dstrip=true &> "$workdir"/meson_log
 
 	echo "Compiling build files ..." $'\n'
 	ninja -C build-android-aarch64 &> "$workdir"/ninja_log
