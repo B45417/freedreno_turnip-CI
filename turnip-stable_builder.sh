c82ea995579fafd56a7f622fc24888fdf8106a5a
B45417 <162130704+B45417@users.noreply.github.com>
2025-03-31 14:59:10 +0200

    Update turnip-stable_builder.sh

--- turnip-stable_builder.sh
+++ turnip-stable_builder.sh
@@ -39,13 +39,14 @@ base_patches=(
         "ir3_b;merge_requests/34110;"
 )
 experimental_patches=(
-        #"bin_merging;merge_requests/33230;"
-        #"bin_merging_fixes;merge_requests/33534;"
-	#"more_bin_merging_fixes;merge_requests/34196;"
-        #"r8g8;merge_requests/32619;"
+        "bin_merging;merge_requests/33230;"
+        "bin_merging_fixes;merge_requests/33534;"
+	"more_bin_merging_fixes;merge_requests/34196;"
         "lrz_fix;merge_requests/33080;"
         "ir3_9;merge_requests/33183;"
 	"bypass_empty_fs;merge_requests/33735;"
+        #"shader_clock;merge_requests/29860;"
+	#"r8g8;merge_requests/32619;"
 )
 failed_patches=()
 commit=""
