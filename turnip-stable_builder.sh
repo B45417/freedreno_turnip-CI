ad6c4d39d89cf1b977599ea98957e810c86cf9bf
B45417 <162130704+B45417@users.noreply.github.com>
2025-04-03 03:34:53 +0200

    checking

--- turnip-stable_builder.sh
+++ turnip-stable_builder.sh
@@ -170,7 +170,8 @@ apply_patches() {
 		patch_source="$(echo $patch | cut -d ";" -f 2 | xargs)"
 		patch_args=$(echo $patch | cut -d ";" -f 3 | xargs)
 		if [[ $patch_source == *"../.."* ]]; then
-			if git apply $patch_args "$patch_source"; then
+			if git apply $patch_args "$patch_source" --check; then
+                                git apply $patch_args "$patch_source"
 				echo "Patch applied successfully"
 			else
 				echo "Failed to apply $patch"
