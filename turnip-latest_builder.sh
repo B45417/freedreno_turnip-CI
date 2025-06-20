01fc3182b1d5a872fa1676dd147ce64f836daedc
B45417 <162130704+B45417@users.noreply.github.com>
2025-06-20 08:41:22 +0200

    Update turnip-latest_builder.sh

--- turnip-latest_builder.sh
+++ turnip-latest_builder.sh
@@ -17,7 +17,6 @@ base_patches=(
         "spirv;merge_requests/35532;"
 	"vk;merge_requests/35594;"
         "copy_raw;merge_requests/35610;"
-	"nir_opt_uniform_subgroup;merge_requests/35636;"
         #'disable_VK_KHR_workgroup_memory_explicit_layout;../../patches/disable_KHR_workgroup_memory_explicit_layout.patch;'
 )
 experimental_patches=(
