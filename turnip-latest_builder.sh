a74dd969a6ce9331fba4b5427f2c4859b40e7c5e
B45417 <162130704+B45417@users.noreply.github.com>
2025-07-04 17:09:36 +0200

    Update turnip-latest_builder.sh

--- turnip-latest_builder.sh
+++ turnip-latest_builder.sh
@@ -21,7 +21,7 @@ base_patches=(
         #'disable_VK_KHR_workgroup_memory_explicit_layout;../../patches/disable_KHR_workgroup_memory_explicit_layout.patch;'
 )
 experimental_patches=(
-        "ir3;merge_requests/34397;--reverse"
+        "copy_raw;merge_requests/35610;"
         #"force_sysmem_no_autotuner;../../patches/force_sysmem_no_autotuner.patch;"
 )
 failed_patches=()
