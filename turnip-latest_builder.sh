7e512ed779d3443eada744cca9b1a5b151f7d455
B45417 <162130704+B45417@users.noreply.github.com>
2025-08-12 17:16:23 +0200

    Update turnip-latest_builder.sh

--- turnip-latest_builder.sh
+++ turnip-latest_builder.sh
@@ -13,14 +13,16 @@ mesasrc="https://gitlab.freedesktop.org/mesa/mesa.git"
 #array of string => commit/branch;patch args
 base_patches=(
         "ir3_sp;merge_requests/36614;"
-	    "ir3_fix;merge_requests/36639;"
 	    "ir3_spill;merge_requests/36667;"
 	    "ir3_ldc;merge_requests/36674;"
+	    "ir3_ssa;merge_requests/36714;"
+	    "ir3_array;merge_requests/36720;"
+	    "spirv;merge_requests/36729;"
 	    "tu_lrz;merge_requests/36660;"
         #'disable_VK_KHR_workgroup_memory_explicit_layout;../../patches/disable_KHR_workgroup_memory_explicit_layout.patch;'
 )
 experimental_patches=(
-        "copy_raw;merge_requests/35610;"
+        #"copy_raw;merge_requests/35610;"
 	    #"tu_cncbinning;merge_requests/36590;"
         #"force_sysmem_no_autotuner;../../patches/force_sysmem_no_autotuner.patch;"
 )
