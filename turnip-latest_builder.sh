f0137c280f051f1542b6448545658321662572d0
B45417 <162130704+B45417@users.noreply.github.com>
2025-08-25 23:39:59 +0200

    Update turnip-latest_builder.sh

--- turnip-latest_builder.sh
+++ turnip-latest_builder.sh
@@ -12,25 +12,12 @@ mesasrc="https://gitlab.freedesktop.org/mesa/mesa.git"
 
 #array of string => commit/branch;patch args
 base_patches=(
-        "test;merge_requests/36907;--reverse"
-        "test;merge_requests/34605;--reverse"
-        "test;merge_requests/36411;--reverse"
-        "test;merge_requests/36819;--reverse"
-        "test;merge_requests/36856;--reverse"
-        "test;merge_requests/36093;--reverse"
-        "test;merge_requests/36821;--reverse"
 		"tu_buffer;merge_requests/36939;"
         "tu_wsi;merge_requests/36948;"
 	    "ir3_spill;merge_requests/36667;"
         #'disable_VK_KHR_workgroup_memory_explicit_layout;../../patches/disable_KHR_workgroup_memory_explicit_layout.patch;'
 )
 experimental_patches=(
-        "test;merge_requests/36929;--reverse"
-        "test;merge_requests/36923;--reverse"
-        "test;merge_requests/36866;--reverse"
-        "test;merge_requests/36827;--reverse"
-        "test;merge_requests/36940;--reverse"
-        "test;merge_requests/36603;--reverse"
 		#"tu_sbr;merge_requests/32533;"
         #"copy_raw;merge_requests/35610;"
 	    #"tu_cncbinning;merge_requests/36590;"
