27321e57f1b2bcd83474d2027a123465a1f22919
B45417 <162130704+B45417@users.noreply.github.com>
2025-10-22 10:41:11 +0200

    Update turnip-latest_builder.sh

--- turnip-latest_builder.sh
+++ turnip-latest_builder.sh
@@ -12,7 +12,6 @@ mesasrc="https://gitlab.freedesktop.org/mesa/mesa.git"
 
 #array of string => commit/branch;patch args
 base_patches=(
-        "vk_mem;merge_requests/37297;"
 		"tu_vk;merge_requests/37990;"
         #"copy_raw;merge_requests/35610;"
 	    #"tu_cncbinning;merge_requests/36590;"
