8ee380c7d240433a70fcfdc275c2de622c374e41
B45417 <162130704+B45417@users.noreply.github.com>
2026-02-26 18:29:41 +0100

    Update turnip-latest_builder.sh

--- turnip-latest_builder.sh
+++ turnip-latest_builder.sh
@@ -22,10 +22,10 @@ base_patches=(
 experimental_patches=(
         #"copy_raw;merge_requests/35610;"
 		#"tu_autotune;merge_requests/37802;"
-		"tu_subsampled;merge_requests/39868;"
+		#"tu_subsampled;merge_requests/39868;"
 		#"tu_fdm/gen8;merge_requests/39789;"
 		#"tu_kgsl/gen8;merge_requests/39751;"
-        #"force_sysmem_no_autotuner;../../patches/force_sysmem_no_autotuner.patch;"
+        "force_sysmem_no_autotuner;../../patches/force_sysmem_no_autotuner.patch;"
 )
 failed_patches=()
 commit=""
