966c2c751ae8914ca8945a95dd6180f1bdfbade8
B45417 <162130704+B45417@users.noreply.github.com>
2026-03-06 18:41:10 +0100

    Update turnip-latest_builder.sh

--- turnip-latest_builder.sh
+++ turnip-latest_builder.sh
@@ -16,6 +16,9 @@ base_patches=(
 		"tu_direct;merge_requests/38960;"
 		"vk_barrier;merge_requests/38956;"
 		"spirv;merge_requests/40017;"
+		"tu_fixes;merge_requests/40264;"
+		"vk_shader;merge_requests/40266;"
+		"vk_cleanup;merge_requests/40268;"
         #'disable_VK_KHR_workgroup_memory_explicit_layout;../../patches/disable_KHR_workgroup_memory_explicit_layout.patch;'
 )
 experimental_patches=(
@@ -23,8 +26,8 @@ experimental_patches=(
 		#"tu_autotune;merge_requests/37802;"
 		#"tu_fdm/gen8;merge_requests/39789;"
 		#"tu_kgsl/gen8;merge_requests/39751;"
-		"tu_subsampled;merge_requests/39868;"
-        #"force_sysmem_no_autotuner;../../patches/force_sysmem_no_autotuner.patch;"
+		#"tu_subsampled;merge_requests/39868;"
+        "force_sysmem_no_autotuner;../../patches/force_sysmem_no_autotuner.patch;"
 )
 failed_patches=()
 commit=""
