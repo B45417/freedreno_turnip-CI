9d1eb32184beb88cc92b4a9297519cbe751246f6
B45417 <162130704+B45417@users.noreply.github.com>
2026-04-05 12:44:41 +0200

    Update turnip-latest_builder.sh

--- turnip-latest_builder.sh
+++ turnip-latest_builder.sh
@@ -177,7 +177,7 @@ build_lib_for_android(){
 	cat <<EOF >"android-aarch64"
 [binaries]
 ar = '$ndk/llvm-ar'
-c = ['ccache', '$ndk/aarch64-linux-android$sdkver-clang']
+c = ['ccache', '$ndk/aarch64-linux-android$sdkver-clang', '-static-libstdc++']
 cpp = ['ccache', '$ndk/aarch64-linux-android$sdkver-clang++', '-fno-exceptions', '-fno-unwind-tables', '-fno-asynchronous-unwind-tables', '--start-no-unused-arguments', '-static-libstdc++', '--end-no-unused-arguments']
 c_ld = '$ndk/ld.lld'
 cpp_ld = '$ndk/ld.lld'
@@ -204,7 +204,6 @@ EOF
   		-Dfreedreno-kmds=kgsl \
 		-Ddefault_library=shared \
         -Dzstd=disabled \
-        -Dwerror=false \
         -Dstrip=true &> "$workdir"/meson_log
 
 	echo "Compiling build files ..." $'\n'
