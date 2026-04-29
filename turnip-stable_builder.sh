61202203511b7ec7f628cd5a719fd5f80be0819b
B45417 <162130704+B45417@users.noreply.github.com>
2026-04-29 22:25:25 +0200

    Update turnip-stable_builder.sh

--- turnip-stable_builder.sh
+++ turnip-stable_builder.sh
@@ -9,9 +9,9 @@ packagedir="$workdir/turnip_module"
 ndkver="android-ndk-r29"
 sdkver="36"
 cver="35"
-mesasrc="https://archive.mesa3d.org/mesa-26.0.5.tar.xz"
-mesadir="mesa-26.0.5"
-mesaver="26.0.5"
+mesasrc="https://archive.mesa3d.org/mesa-26.0.6.tar.xz"
+mesadir="mesa-26.0.6"
+mesaver="26.0.6"
 
 #array of string => commit/branch;patch args
 base_patches=(
