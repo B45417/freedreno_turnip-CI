b327da0e71e4a048ce3d808b3040571f0ca90404
B45417 <162130704+B45417@users.noreply.github.com>
2026-05-28 12:54:33 +0200

    Update turnip-latest_builder.sh

--- turnip-latest_builder.sh
+++ turnip-latest_builder.sh
@@ -21,11 +21,11 @@ base_patches=(
 		"wsi_immediate;merge_requests/40584;"
 		"wsi_fix;merge_requests/41188;"
 		"wsi_buffers;merge_requests/36266;"
+		"tu_fragfc7xx;merge_requests/41848;"
         #'disable_VK_KHR_workgroup_memory_explicit_layout;../../patches/disable_KHR_workgroup_memory_explicit_layout.patch;'
 )
 experimental_patches=(
         "vk_antilag;merge_requests/41727;"
-		#"tu_ubwc6xx;merge_requests/39491;"
         #"copy_raw;merge_requests/35610;"
         #"force_sysmem_no_autotuner;../../patches/force_sysmem_no_autotuner.patch;"
 )
