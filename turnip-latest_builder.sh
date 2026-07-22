23172760cd34720e51e9995c5d5278aef91a7a3b
B45417 <162130704+B45417@users.noreply.github.com>
2026-07-22 19:39:36 +0200

    Update turnip-latest_builder.sh

--- turnip-latest_builder.sh
+++ turnip-latest_builder.sh
@@ -16,9 +16,7 @@ driver="vulkan.turnip.so"
 base_patches=(
 		"vk;merge_requests/38323;"
 		"tu_rrg;merge_requests/43018;"
-		"tu_autotune;merge_requests/42953;"
 		"tu_compress;merge_requests/42489;"
-		"tu_aprons;merge_requests/43120;"
         #'disable_VK_KHR_workgroup_memory_explicit_layout;../../patches/disable_KHR_workgroup_memory_explicit_layout.patch;'
 )
 experimental_patches=(
