d2049a5cccba6bc1078b318e125b96fbeea2ed56
B45417 <162130704+B45417@users.noreply.github.com>
2026-08-07 16:38:38 +0200

    Update turnip-latest_builder.sh

--- turnip-latest_builder.sh
+++ turnip-latest_builder.sh
@@ -16,7 +16,7 @@ driver="vulkan.turnip.so"
 base_patches=(
 		"vk;merge_requests/38323;"
 		"tu_compress;merge_requests/42489;"
-		"tu_rndrinput;merge_requests/43165;"
+		#"tu_rndrinput;merge_requests/43165;"
 		"tu_sync;merge_requests/43272;"
 		"tu_lrz;merge_requests/43271;"
 		"tu_lrz;merge_requests/43540;"
