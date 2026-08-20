e52450ef78d42e02db775288712f3366a0d88e4c
B45417 <162130704+B45417@users.noreply.github.com>
2026-08-20 09:02:00 +0200

    Update turnip-stable_builder.sh

--- turnip-stable_builder.sh
+++ turnip-stable_builder.sh
@@ -9,7 +9,7 @@ packagedir="$workdir/turnip_module"
 ndkver="android-ndk-r29"
 sdkver="36"
 cver="35"
-mesaver="26.1.7"
+mesaver="26.1.8"
 mesadir="mesa-$mesaver"
 mesasrc="https://archive.mesa3d.org/$mesadir.tar.xz"
 
