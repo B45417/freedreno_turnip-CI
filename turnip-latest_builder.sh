647a91b86052fff3133494dfd0b779a32ce6dd18
B45417 <162130704+B45417@users.noreply.github.com>
2026-09-10 20:34:51 +0200

    Update turnip-latest_builder.sh

--- turnip-latest_builder.sh
+++ turnip-latest_builder.sh
@@ -196,7 +196,6 @@ EOF
    		-Dplatforms=android \
      	-Dplatform-sdk-version=$sdkver \
        	-Dandroid-stub=true \
-		-Dandroid-libbacktrace=disabled \
 		-Degl=disabled \
 	 	-Dgallium-drivers= \
   		-Dvulkan-drivers=freedreno \
