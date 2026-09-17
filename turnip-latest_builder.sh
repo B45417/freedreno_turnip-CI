aec87cff489a45ecd3955f4481fcaf27ed6ad841
B45417 <162130704+B45417@users.noreply.github.com>
2026-09-17 07:24:43 +0200

    Update turnip-latest_builder.sh

--- turnip-latest_builder.sh
+++ turnip-latest_builder.sh
@@ -15,7 +15,6 @@ driver="vulkan.turnip.so"
 #array of string => commit/branch;patch args (ex --reverse)
 base_patches=(
 		"sysmem;merge_requests/43899;"
-		"binding;merge_requests/44436;"
         #"disable_VK_KHR_workgroup_memory_explicit_layout;../../patches/disable_KHR_workgroup_memory_explicit_layout.patch;"
 )
 experimental_patches=(
